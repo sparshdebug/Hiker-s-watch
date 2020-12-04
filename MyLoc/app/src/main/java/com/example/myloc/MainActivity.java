c596a1220321ee19ebb1c49dab3b82c57188b618
Sparsh <sparshxtaneja@gmail.com>
2020-12-04 13:13:11 +0530

    location second commit

--- MyLoc/app/src/main/java/com/example/myloc/MainActivity.java
+++ MyLoc/app/src/main/java/com/example/myloc/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.myloc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -16,7 +20,18 @@ public class MainActivity extends AppCompatActivity {
         ListView listView = findViewById(R.id.listView);
         ArrayList<String> places = new ArrayList<>();
         places.add("Click here to add..");
-        
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,places);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 1) {
+                    Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
 
     }
 }
