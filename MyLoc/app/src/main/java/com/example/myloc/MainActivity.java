73d30d355d3fbb64cce4eae99c819a8ea2722752
Sparsh <sparshxtaneja@gmail.com>
2020-12-04 12:54:51 +0530

    location second commit

--- MyLoc/app/src/main/java/com/example/myloc/MainActivity.java
+++ MyLoc/app/src/main/java/com/example/myloc/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myloc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView listView = findViewById(R.id.listView);
+        ArrayList<String> places = new ArrayList<>();
+        places.add("Click here to add..");
+        
+
     }
 }
